aa872e78efef35c5bd32f729832087a334f58aee
Eddy Rodriguez <e.rodriguezd@pucp.edu.pe>
2020-07-05 21:04:33 -0500

    entities

---
+++ app/src/main/java/com/lab4moviles/entities/Comentario.java
@@ -0,0 +1,36 @@
+package com.lab4moviles.entities;
+
+import java.io.Serializable;
+import java.util.Date;
+import java.util.Map;
+
+public class Comentario implements Serializable {
+
+    private Map<String, String> usuario;
+    private Date fecha;
+    private String texto;
+
+    public Map<String, String> getUsuario() {
+        return usuario;
+    }
+
+    public void setUsuario(Map<String, String> usuario) {
+        this.usuario = usuario;
+    }
+
+    public Date getFecha() {
+        return fecha;
+    }
+
+    public void setFecha(Date fecha) {
+        this.fecha = fecha;
+    }
+
+    public String getTexto() {
+        return texto;
+    }
+
+    public void setTexto(String texto) {
+        this.texto = texto;
+    }
+}
